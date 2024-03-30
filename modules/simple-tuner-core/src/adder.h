96dd1e952cec034f767c59b70d772b766adf7dde
Ben MERTZ <ben.mertz.dev@gmail.com>
2024-03-30 20:44:35 +0100

    chore(init): workflow init

---
+++ modules/simple-tuner-core/src/adder.h
@@ -0,0 +1,16 @@
+//
+// Created by Ben MERTZ on 30/03/2024.
+//
+
+#ifndef CPP_TRAINING_JUCE_TUNER_ADDER_H
+#define CPP_TRAINING_JUCE_TUNER_ADDER_H
+
+
+
+class adder {
+
+};
+
+
+
+#endif //CPP_TRAINING_JUCE_TUNER_ADDER_H
